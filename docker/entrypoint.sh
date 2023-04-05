9de61c528d6e2c575c6e345fc006e5825b013b1d
felix ayange <felixdecoder2020@gmail.com>
2023-04-05 09:10:39 +0100

    initial commit

---
+++ docker/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+    php artisan key:generate
+else
+    echo "env file exists."
+fi
+
+php artisan config:clear
+php artisan migrate --force --seed
+php artisan clear-compiled
+php artisan auth:clear-resets
+php artisan optimize:clear
+php artisan config:cache
+php artisan view:cache
+php artisan route:cache
+composer dump-autoload
+
+
+php-fpm -D
+nginx -g "daemon off;"
